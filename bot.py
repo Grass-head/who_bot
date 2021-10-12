2967cb75f32bbbc7a2379d761888ecd74da3ac55
Kseniya Bagnyuk <kseniyabagnyuk@gmail.com>
2021-10-12 18:30:02 +0700

    logic of cards display changed, text promptfor user and comments added

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# Импортируем нужные компоненты
+#импортируем нужные компоненты
 from glob import glob
 import logging
 from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton, InlineKeyboardButton
@@ -7,19 +7,26 @@ from random import choice, randint
 
 import settings
 
+#добавляем базовое логирование
 logging.basicConfig(filename='bot.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+#добавляем функцию с reply-клавиатурой с основными управляющими кнопками
 def main_keyboard():
-	#Создаем переменную с реплай клавиатурой
+	#создаем переменную, содержащую reply-клавиатуру с кнопками, которая будет выводится всегда
 	replykeyboard= [[
 		KeyboardButton("КАТЕГОРИИ"),
 		KeyboardButton("СТОП-ИГРА")
 		]]
+	#назначаем переменную в которой содержится клавиатура полностью
 	my_keyboard = ReplyKeyboardMarkup(replykeyboard, resize_keyboard = True)
+	#назначаем вывод клавиатуры на экран
 	return my_keyboard
 
+#добавляем функцию с inline-клавиатурой с наваниями категорий, в которых будут выдаваться катрочки
 def get_inline():
+	#создаем переменную-массив inline-кнопок с callback, где каждому словарю(категории) соответствует кнопка с назанием
 	inlinekeyboard= [
+		#создаем отдельную кнопку для каждой категории внутри массива
 		[InlineKeyboardButton('ГОРОДА', callback_data = "city")],
 		[InlineKeyboardButton('ЖИВОТНЫЕ', callback_data = "animal")],
 		[InlineKeyboardButton('ПРЕДМЕТЫ БЫТА', callback_data = "houseware")],
@@ -31,75 +38,95 @@ def get_inline():
 		[InlineKeyboardButton('РАСТЕНИЯ', callback_data = "plants")],
 		[InlineKeyboardButton('ДОСТОПРИМЕЧАТЕЛЬНОСТИ', callback_data = "sightseeings")]
 		]
+	#назначаем переменную в которой содержится клавиатура полностью
 	my_inline_keyboard = InlineKeyboardMarkup(inlinekeyboard)
+	#вывод клавиатуры на экран
 	return my_inline_keyboard
 
-#Создаем функцию, которая будет приветствовать пользователя при нажатии конпки start
+#создаем функцию, которая будет приветствовать пользователя при нажатии конпки start
 def greet_user(update, context):
-	#В терминал будет выводится инфо о пользователе
-	print(update.message)
-	#Собираем информацию "имя пользователя" и сохраняем в переменную для последующего вывода
+	#собираем информацию "имя пользователя" и сохраняем в переменную для последующего вывода
 	name = update.message.chat.first_name
-	#Создаем переменную с приветственным текстом для последующего вывода
-	text = "Привет, {}! Данный бот поможет, если нет ручки и бумаги. Просто следуй подсказкам бота, выбирай тематику и начинай играть. Если хочешь продолжить игру нажми кнопку КАТЕГОРИИ. Чтобы отсановить бота нажми СТОП-ИГРА".format(name)
-	#На экране выводится приветственное сообщение
-	update.message.reply_text(text, reply_markup = main_keyboard())
+	#создаем переменные с приветственным текстом и подсказками для последующего вывода
+	txt_greet = "Привет, {}! Данный бот поможет, если под рукой нет ручки и бумаги. Просто следуй подсказкам бота, выбирай тематику и начинай играть.".format(name)
+	txt_prompt1 = "Чтобы играть было удобнее, увеличь время до перехода телефона в спящий режим или вовсе отключи авто-переход в спящий режим на время игры."
+	txt_prompt2 = "Отключи блокировку книжной ориентации и режим энергосбережения на телефоне."
+	txt_choice = "Если хочешь продолжить игру нажми кнопку КАТЕГОРИИ. Чтобы остановить бота нажми СТОП-ИГРА."
+	#вывод приветственного сообщения на экран
+	update.message.reply_text(txt_greet)
+	#вывод сообщений-подсказок на экран
+	update.message.reply_text(txt_prompt1)
+	update.message.reply_text(txt_prompt2)
+	#вывод сообщения-рычага и основоной клавиатуры на экран
+	update.message.reply_text(txt_choice, reply_markup = main_keyboard())
 
-def stop_game(update, context):
-	update.message.reply_text("Жаль, что уже уходишь! Приходи исчо!")
-	#updater.stop()
-
+#создаем функцию выбора категорий, которая проинструктирует юзера о дальнейших действиях и выдаст клавиатуру с категориями
 def categories(update, context):
-	txt_choice = "Выберите одну из приведенных ниже категорий. Все игроки в комнате выбирают одинаковую категорию."
-	update.message.reply_text(txt_choice, reply_markup = get_inline())
+	#создаем переменные с подсказками для последующего вывода
+	txt_choice1 = "Выбери одну из категорий, которые будут предложены чуть позже. Все игроки в комнате выбирают одинаковую категорию."
+	txt_prompt3 = "Когда выбор будет сделан, ты получишь случайную карточку. Содержимое будет видно через 5 секунд после загрузки."
+	txt_prompt4 = "Щелкни по карточке, активируя фулл-скрин показ. Далее переверни телефон в горизонтальное положение экраном от себя."
+	txt_choice2 = "Выбери вариант и наслаждайся игрой."
+	#вывод сообщений-подсказок на экран
+	update.message.reply_text(txt_choice1)
+	update.message.reply_text(txt_prompt3)
+	update.message.reply_text(txt_prompt4)
+	#вывод сообщения-рычага и инлайн-клавиатуры на экран
+	update.message.reply_text(txt_choice2, reply_markup = get_inline())
 
-
+#добавим функцию, которая анализирует callback с кнопки, нажатой пользователем, и выводит карточку соответсвующей категории
 def send_card(update, context):
-	query = update.callback_query
+	#назначаем переменную и собираем в нее данные callback с кнопок клавиатуры
 	query_data = update.callback_query.data
+	#добавляем условный оператор, чтобы задать направление движения программы в соответствии с выбранной юзером категорией
 	if query_data == "city":
-		cards_list = glob('cards/cities/*.png');
+		#назначаем переменную с path, по которому будет осуществляться поиск карточки(для каждой категории)
+		cards_list = glob('cards/cities/*.gif');
 	elif query_data == "animal":
-		cards_list = glob('cards/animals/*.png');
+		cards_list = glob('cards/animals/*.gif');
 	elif query_data == "houseware":
-		cards_list = glob('cards/houseware/*.png');
+		cards_list = glob('cards/houseware/*.gif');
 	elif query_data == "actors":
-		plug = glob('cards/actors/1/*.png')
-		cards_list = glob('cards/actors/*.png');
+		cards_list = glob('cards/actors/*.gif');
 	elif query_data == "food":
-		cards_list = glob('cards/food/*.png');
+		cards_list = glob('cards/food/*.gif');
 	elif query_data == "politicians":
-		cards_list = glob('cards/politicians/*.png');
+		cards_list = glob('cards/politicians/*.gif');
 	elif query_data == "superheroes":
-		cards_list = glob('cards/superheroes/*.png');
+		cards_list = glob('cards/superheroes/*.gif');
 	elif query_data == "plants":
-		cards_list = glob('cards/plants/*.png');
+		cards_list = glob('cards/plants/*.gif');
 	elif query_data == "sightseeings":
-		cards_list = glob('cards/sightseeings/*.png');
+		cards_list = glob('cards/sightseeings/*.gif');
 	else:
-		cards_list = glob('cards/profession/*.png')
-	plug1 = choice(glob('cards/actors/1/*.png'))
-	cardname = choice(cards_list)
+		cards_list = glob('cards/profession/*.gif')
+	#назначаем переменную с id чата из данных, полученных от пользователя, она нам понадобится для отправки карточки
 	chat_id = update.effective_chat.id
-	query.edit_message_text("Отключите блокировку книжной ориентации на телефоне, переверните телефон в горизонтальное положение экраном от себя, а также увеличьте время до перехода телефона в спящий режим или вовсе отключите спящй режим на время игры")
-	context.bot.send_photo(chat_id=chat_id, photo=open(plug1, "rb"))
-	context.bot.edit_message_media(chat_id=chat_id, message_id = plug1, media = cardname)
+	#назначаем переменную с рандомным выбором карточки из соответствующей папки
+	cardname = choice(cards_list)
+	#отправляем выбранную рандомно ранее карточку пользователю
+	context.bot.send_animation(chat_id=chat_id, animation=open(cardname, "rb"))
 
-
+#создаем функцию принудительной остановки бота
+def stop_game(update, context):
+	#вывод на экран сообщения с прощальным текстом
+	update.message.reply_text("Жаль, что уже уходишь! Заглядывай еще!")
+	#операция принудительной остановки бота
+	updater.stop()
 
 def main():
-	# Создаем бота и передаем ему ключ для авторизации на серверах Telegram
+	#создаем бота и передаем ему ключ для авторизации на серверах Telegram
   mybot = Updater(settings.API_KEY, use_context=True)
-  #Добавляем диспетчер, который будет реагировать на стратовое событие
+  #добавляем диспетчер, который будет реагировать на событие
   dp = mybot.dispatcher
+  #добавляем хендлеры для обработки событий
   dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
-  dp.add_handler(CallbackQueryHandler(send_card))
+  dp.add_handler(CallbackQueryHandler(send_card, pass_user_data=True)))
   dp.add_handler(MessageHandler(Filters.regex('^(СТОП-ИГРА)$'), stop_game, pass_user_data=True))
   dp.add_handler(MessageHandler(Filters.regex('^(КАТЕГОРИИ)$'), categories, pass_user_data=True))
-  # Командуем боту начать ходить в Telegram за сообщениями
-  logging.info("Бот стартовал")
+  #командуем боту начать ходить в Telegram за сообщениями
   mybot.start_polling()
-  # Запускаем бота, он будет работать, пока мы его не остановим принудительно
+  #запускаем бота, он будет работать, пока мы его не остановим принудительно
   mybot.idle()
 
 
